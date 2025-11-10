d5bfd05bc94e8218a29e420524e557937035434e
Nayla Quinsha Nabila <nayla.quinshanabila@gmail.com>
2025-11-10 17:23:59 +0700

    tambah model dan index kelas

---
+++ app/Models/Siswa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $fillable = [
+        'kelas_id',
+        'nama',
+        'jenis_kelamin',
+        'tanggal_lahir'
+    ];
+}
--- resources/views/kelas/index.blade.php
+++ resources/views/kelas/index.blade.php
@@ -1,3 +1,38 @@
-<div>
-    <!-- It always seems impossible until it is done. - Nelson Mandela -->
-</div>
+@extends('layout')
+
+@section('title')
+Data kelas
+@endsection
+
+@section('content')
+<a href="{{ url('kelas/create') }}" class="btn btn-primary">
+    Tambah kelas
+</a>
+<table class="table table-striped table-hover table-bordered mt-3">
+    <thead>
+        <tr>
+            <td class="text-center"></td>
+            <td class="text-center"></td>
+            <td class="text-center"></td>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- isi data -->
+         @php $i = 1; @endphp
+        @foreach($kelas as $Kelas)
+        <tr>
+            <td>{{ $i++ }}</td>
+            <td>{{ $Kelas->name }}</td>
+            <td class="text-center">
+                <a href="{{ url('kelas/'.$kelas->$id.'/edit') }}" class="btn btn-primary">
+                    Edit
+                </a>
+                <a href="{{ url('kelas/'.$kelas->$id.'/delete') }}" class="btn btn-danger">
+                    Hapus
+                </a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
